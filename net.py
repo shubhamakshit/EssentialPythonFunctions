ec1a291a7ea533d5af150450795fb77d726a67de
Akshit Shubham <akshitshubhammas@gmail.com>
2024-04-24 20:37:54 +0530

    Update net.py

--- net.py
+++ net.py
@@ -1,4 +1,5 @@
 import subprocess
+import socket
 
 class NetUtils:
     @staticmethod
@@ -15,11 +16,15 @@ class NetUtils:
 
     @staticmethod
     def server(ipv4_address, SERVER_PORT, FOLDER_MAIN="./"):
+        # Check if the specified port is already in use
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((ipv4_address, SERVER_PORT))
+        if result == 0:
+            print(f"Port {SERVER_PORT} is already in use. Server cannot be started.")
+            return
+
         # Starting a simple HTTP server on a specified port and directory
         subprocess.Popen(['python', '-m', 'http.server', str(SERVER_PORT), '--directory', FOLDER_MAIN], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        SERVER_STARTED = True  # Assuming the server started successfully
-        with open('server.prop.state', 'w') as f:
-            f.write('1')
         print(f'Server started at http://{ipv4_address}:{SERVER_PORT}')
 
 
